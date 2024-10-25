5ef21992cf4a78d6b64493b56eb0dfd53c7c54a1
KatayR <rasitarslan@outlook.com>
2024-10-25 11:24:45 +0300

    created models

---
+++ lib/models/meal_model.dart
@@ -0,0 +1,65 @@
+class Meal {
+  final String idMeal;
+  final String strMeal;
+  final String? strDrinkAlternate;
+  final String strCategory;
+  final String strArea;
+  final String strInstructions;
+  final String strMealThumb;
+  final String strTags;
+  final String strYoutube;
+  final List<String> ingredients;
+  final List<String> measures;
+
+  Meal({
+    required this.idMeal,
+    required this.strMeal,
+    this.strDrinkAlternate,
+    required this.strCategory,
+    required this.strArea,
+    required this.strInstructions,
+    required this.strMealThumb,
+    required this.strTags,
+    required this.strYoutube,
+    required this.ingredients,
+    required this.measures,
+  });
+
+  factory Meal.fromJson(Map<String, dynamic> json) {
+    List<String> extractIngredients(Map<String, dynamic> json) {
+      List<String> ingredients = [];
+      for (int i = 1; i <= 20; i++) {
+        String ingredient = json['strIngredient$i'] ?? '';
+        if (ingredient.isNotEmpty) {
+          ingredients.add(ingredient);
+        }
+      }
+      return ingredients;
+    }
+
+    List<String> extractMeasures(Map<String, dynamic> json) {
+      List<String> measures = [];
+      for (int i = 1; i <= 20; i++) {
+        String measure = json['strMeasure$i'] ?? '';
+        if (measure.isNotEmpty) {
+          measures.add(measure);
+        }
+      }
+      return measures;
+    }
+
+    return Meal(
+      idMeal: json['idMeal'],
+      strMeal: json['strMeal'],
+      strDrinkAlternate: json['strDrinkAlternate'],
+      strCategory: json['strCategory'],
+      strArea: json['strArea'],
+      strInstructions: json['strInstructions'],
+      strMealThumb: json['strMealThumb'],
+      strTags: json['strTags'] ?? '',
+      strYoutube: json['strYoutube'] ?? '',
+      ingredients: extractIngredients(json),
+      measures: extractMeasures(json),
+    );
+  }
+}
