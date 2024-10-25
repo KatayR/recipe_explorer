1a3f38f3594b64dbaf77e55dfd98b867718fe3bc
KatayR <rasitarslan@outlook.com>
2024-10-25 17:09:13 +0300

    ...with this!

---
+++ lib/utils/responsive_helper.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveHelper {
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 600;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 600 &&
+      MediaQuery.of(context).size.width < 1200;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1200;
+
+  static int getGridCrossAxisCount(BuildContext context) {
+    if (isMobile(context)) return 1;
+    if (isTablet(context)) return 2;
+    return 3;
+  }
+
+  static double getGridChildAspectRatio(BuildContext context) {
+    if (isMobile(context)) return 0.85;
+    if (isTablet(context)) return 0.75;
+    return 0.7;
+  }
+}
