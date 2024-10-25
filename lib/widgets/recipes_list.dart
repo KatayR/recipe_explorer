e7ce55faebdd2a38467a5a30c27f2a41118f917d
KatayR <rasitarslan@outlook.com>
2024-10-25 11:44:41 +0300

    Add RecipeList widget for displaying a list of meals with images and names

---
+++ lib/widgets/recipes_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class RecipeList extends StatelessWidget {
+  final List<dynamic> meals;
+
+  RecipeList({required this.meals});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (context, index) {
+        final meal = meals[index];
+        return ListTile(
+          title: Text(meal['strMeal']),
+          leading: Image.network(meal['strMealThumb']),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => RecipeDetail(mealName: meal["strMeal"]),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
