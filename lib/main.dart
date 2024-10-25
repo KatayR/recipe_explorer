ac1e57b37aff4dacbbf5a4e61ec3cfaa672a8c28
KatayR <rasitarslan@outlook.com>
2024-10-25 11:20:06 +0300

    Initial with basic folder structure

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:../screens/home_page.dart';
+
+void main() {
+  runApp(const RecipeExplorer());
+}
+
+class RecipeExplorer extends StatelessWidget {
+  const RecipeExplorer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe Explorer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
