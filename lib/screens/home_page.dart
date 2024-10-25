8d160c2c6450211ccee958ac02034c4e2a1e8069
KatayR <rasitarslan@outlook.com>
2024-10-25 17:26:37 +0300

    Implemented filtered search feature

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,35 +10,39 @@ import '../widgets/meal_list/meal_grid.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final TextEditingController _searchController = TextEditingController();
   final MealService _mealService = MealService();
   List<Category> _categories = [];
+  bool _showSearch = false;
 
-  void _searchMeals(String query, SearchType type) {
-    if (query.isNotEmpty) {
+  void _searchMeals(String query, SearchOptions searchOptions) {
+    if (query.trim().isNotEmpty) {
       Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => ResultsPage(
             searchQuery: query,
-            searchType: type,
+            searchOptions: searchOptions,
           ),
         ),
       );
     }
   }
 
-  void _loadCategories() async {
-    final categories = await _mealService.getCategories();
-    setState(() {
-      _categories = categories.map((json) => Category.fromJson(json)).toList();
-    });
+  Future<void> _loadCategories() async {
+    try {
+      final categories = await _mealService.getCategories();
+      setState(() {
+        _categories =
+            categories.map((json) => Category.fromJson(json)).toList();
+      });
+    } catch (e) {
+      print('Error loading categories: $e');
+    }
   }
 
   void _onCategorySelected(String category) {
@@ -61,74 +65,65 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Recipe Explorer'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FavoritesPage()),
+      appBar: AppBar(
+        title: const Text('Recipe Explorer'),
+        actions: [
+          IconButton(
+            icon: Icon(_showSearch ? Icons.close : Icons.search),
+            onPressed: () => setState(() => _showSearch = !_showSearch),
+          ),
+          IconButton(
+            icon: const Icon(Icons.favorite),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => FavoritesPage()),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          CustomSearchBar(
+            isVisible: _showSearch,
+            onSearch: _searchMeals,
+            onClose: () => setState(() => _showSearch = false),
+          ),
+          CategoryList(
+            categories: _categories,
+            onCategorySelected: _onCategorySelected,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Text("Sample dishes you can make with chicken"),
+          ),
+          Expanded(
+            child: FutureBuilder<MealResponse>(
+              future: _mealService.fetchMeals(category: 'Chicken'),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (snapshot.hasError) {
+                  return const Center(
+                    child: Text('Error loading default(chicken) recipes'),
+                  );
+                }
+                return MealGrid(
+                  meals: snapshot.data?.meals ?? [],
+                  onMealSelected: (mealName) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RecipeDetail(mealName: mealName),
+                      ),
+                    );
+                  },
                 );
               },
-              child: const Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text('Favorites'),
-                  SizedBox(width: 8),
-                  Icon(Icons.favorite),
-                ],
-              ),
             ),
-          ],
-        ),
-        body: Column(
-          children: [
-            CustomSearchBar(
-              controller: _searchController,
-              onSearch: _searchMeals,
-            ),
-            CategoryList(
-              categories: _categories,
-              onCategorySelected: _onCategorySelected,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Some chicken recipes',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Expanded(
-              child: FutureBuilder<MealResponse>(
-                future: _mealService.fetchMeals(category: 'Chicken'),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  if (snapshot.hasError) {
-                    return const Center(
-                        child: Text('Error loading default(chicken) recipes'));
-                  }
-                  return MealGrid(
-                    meals: snapshot.data?.meals ?? [],
-                    onMealSelected: (mealName) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              RecipeDetail(mealName: mealName),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
