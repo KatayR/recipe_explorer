94efb72968dac6ace3af1c0c261dd61381cf86f2
KatayR <rasitarslan@outlook.com>
2024-10-25 13:59:01 +0300

    fixed favorite state

--- lib/services/favorires_service.dart
+++ lib/services/favorires_service.dart
@@ -1,21 +1,30 @@
+import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class FavoritesService {
+  static const String _key = 'favorites';
+
   Future<List<String>> getFavorites() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getStringList('favorites') ?? [];
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList(_key) ?? [];
   }
 
-  Future<void> toggleFavorite(String mealId) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> favorites = prefs.getStringList('favorites') ?? [];
+  Future<bool> isFavorite(String mealId) async {
+    final favorites = await getFavorites();
+    return favorites.any((item) => jsonDecode(item)['idMeal'] == mealId);
+  }
 
-    if (favorites.contains(mealId)) {
-      favorites.remove(mealId);
+  Future<void> toggleFavorite(Map<String, dynamic> mealJson) async {
+    final prefs = await SharedPreferences.getInstance();
+    final favorites = await getFavorites();
+    final mealString = jsonEncode(mealJson);
+
+    if (favorites.contains(mealString)) {
+      favorites.remove(mealString);
     } else {
-      favorites.add(mealId);
+      favorites.add(mealString);
     }
 
-    await prefs.setStringList('favorites', favorites);
+    await prefs.setStringList(_key, favorites);
   }
 }
