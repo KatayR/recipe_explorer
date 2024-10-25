2d907dec09577c879abde43b8eb363135c18771b
KatayR <rasitarslan@outlook.com>
2024-10-25 11:34:14 +0300

    Created categories widget

---
+++ lib/widgets/categories_list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../models/category_model.dart';
+
+class CategoryList extends StatelessWidget {
+  final List<Category> categories;
+  final Function(String category) onCategorySelected;
+
+  CategoryList({required this.categories, required this.onCategorySelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          final category = categories[index];
+          return GestureDetector(
+            onTap: () => onCategorySelected(category.strCategory),
+            child: Container(
+              width: 100,
+              margin: EdgeInsets.symmetric(horizontal: 8),
+              child: Column(
+                children: [
+                  Image.network(category.strCategoryThumb, height: 50),
+                  SizedBox(height: 4),
+                  Text(category.strCategory, style: TextStyle(fontSize: 12)),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
