e83b11bd7784142320fa1186ee5ff198d8e71c4d
KatayR <rasitarslan@outlook.com>
2024-10-26 19:45:49 +0300

    Changed sql library to add Windows support

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'screens/home/home_page.dart';
+import 'services/storage_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize the FFI database
+  await StorageService.initializeFfi();
   runApp(const RecipeExplorer());
 }
 
