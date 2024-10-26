28e08815d6664f09d0d37125c20658eac9ca5a02
KatayR <rasitarslan@outlook.com>
2024-10-26 17:36:05 +0300

    Created a cleaner search bar widget

--- lib/widgets/home/custom_search_bar.dart
+++ lib/widgets/home/custom_search_bar.dart
@@ -1,95 +1,37 @@
 import 'package:flutter/material.dart';
 
-class SearchOptions {
-  final bool byName;
-  final bool byIngredient;
-
-  const SearchOptions({
-    required this.byName,
-    required this.byIngredient,
-  });
-
-  bool get hasSelection => byName || byIngredient;
-}
-
 class CustomSearchBar extends StatefulWidget {
-  final Function(String query, SearchOptions searchOptions) onSearch;
+  final Function(String, {bool byName, bool byIngredient}) onSearch;
   final VoidCallback onClose;
-  final bool isVisible;
 
   const CustomSearchBar({
     super.key,
     required this.onSearch,
     required this.onClose,
-    required this.isVisible,
   });
 
   @override
   State<CustomSearchBar> createState() => _CustomSearchBarState();
 }
 
-class _CustomSearchBarState extends State<CustomSearchBar>
-    with SingleTickerProviderStateMixin {
-  late final TextEditingController _controller;
-  late final AnimationController _animationController; // AI magic here
-  late final Animation<double> _animation;
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  final _controller = TextEditingController();
   bool _byName = true;
   bool _byIngredient = false;
-  final FocusNode _focusNode = FocusNode();
-
-  @override
-  void initState() {
-    super.initState();
-    // Only god and the ChatGPT knows how this animation works
-    _controller = TextEditingController();
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 300),
-      vsync: this,
-    );
-    _animation = CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeInOut,
-    );
-
-    if (widget.isVisible) {
-      _animationController.forward();
-    }
-  }
-
-  @override
-  void didUpdateWidget(CustomSearchBar oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.isVisible != oldWidget.isVisible) {
-      if (widget.isVisible) {
-        _animationController.forward();
-        _focusNode.requestFocus();
-      } else {
-        _animationController.reverse();
-      }
-    }
-  }
 
   @override
   void dispose() {
     _controller.dispose();
-    _animationController.dispose();
-    _focusNode.dispose();
     super.dispose();
   }
 
   void _handleSearch() {
-    if (!_byName && !_byIngredient) {
-      setState(() => _byName = true);
-    }
-
     final query = _controller.text.trim();
     if (query.isNotEmpty) {
       widget.onSearch(
         query,
-        SearchOptions(
-          byName: _byName,
-          byIngredient: _byIngredient,
-        ),
+        byName: _byName,
+        byIngredient: _byIngredient,
       );
       _controller.clear();
       widget.onClose();
@@ -98,62 +40,47 @@ class _CustomSearchBarState extends State<CustomSearchBar>
 
   @override
   Widget build(BuildContext context) {
-    return SizeTransition(
-      sizeFactor: _animation,
-      axisAlignment: -1,
-      child: Container(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    focusNode: _focusNode,
-                    decoration: InputDecoration(
-                      hintText: 'Search recipes...',
-                      prefixIcon: const Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: 'Search recipes...',
+                    prefixIcon: const Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    onSubmitted: (_) => _handleSearch(),
                   ),
+                  onSubmitted: (_) => _handleSearch(),
                 ),
-                const SizedBox(width: 8),
-                PopupMenuButton<SearchOptions>(
-                  icon: const Icon(Icons.tune),
-                  onSelected: (SearchOptions options) {
-                    setState(() {
-                      _byName = options.byName;
-                      _byIngredient = options.byIngredient;
-                    });
-                  },
-                  itemBuilder: (BuildContext context) => [
-                    CheckedPopupMenuItem(
-                      value: SearchOptions(
-                        byName: !_byName,
-                        byIngredient: _byIngredient,
-                      ),
-                      checked: _byName,
-                      child: const Text('Search by Name'),
-                    ),
-                    CheckedPopupMenuItem(
-                      value: SearchOptions(
-                        byName: _byName,
-                        byIngredient: !_byIngredient,
-                      ),
-                      checked: _byIngredient,
-                      child: const Text('Search by Ingredient'),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
+              ),
+              const SizedBox(width: 8),
+              PopupMenuButton<void>(
+                icon: const Icon(Icons.tune),
+                itemBuilder: (context) => [
+                  CheckedPopupMenuItem(
+                    value: null,
+                    checked: _byName,
+                    onTap: () => setState(() => _byName = !_byName),
+                    child: const Text('Search by Name'),
+                  ),
+                  CheckedPopupMenuItem(
+                    value: null,
+                    checked: _byIngredient,
+                    onTap: () => setState(() => _byIngredient = !_byIngredient),
+                    child: const Text('Search by Ingredient'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
