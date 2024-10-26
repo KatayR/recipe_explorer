a2cf8e9f8c77fc99520d2af23f9cd017a1fd7a3e
KatayR <rasitarslan@outlook.com>
2024-10-26 20:33:28 +0300

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class RecipeExplorer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       scrollBehavior: const MaterialScrollBehavior().copyWith(
         dragDevices: {
           PointerDeviceKind.mouse,
