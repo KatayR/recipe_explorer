5f901ccc56e0a11d141e56e58c3450f04280dd22
KatayR <rasitarslan@outlook.com>
2024-10-27 21:50:16 +0300

    Clarified error message

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -62,7 +62,7 @@ class _HomePageState extends State<HomePage> {
               SliverFillRemaining(
                 child: Center(
                   child: ErrorView(
-                    errString: 'No internet connection',
+                    errString: "Couldn't load homepage. Check your connection.",
                     onRetry: retryCallback,
                   ),
                 ),
