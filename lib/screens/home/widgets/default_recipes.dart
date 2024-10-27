fa7661db5020887f4d2c49789dd607566942ef4b
KatayR <rasitarslan@outlook.com>
2024-10-27 23:01:50 +0300

    commented

--- lib/screens/home/widgets/default_recipes.dart
+++ lib/screens/home/widgets/default_recipes.dart
@@ -1,3 +1,27 @@
+/// A widget that displays a section of default recipes fetched from an API.
+///
+/// The `DefaultRecipesSection` widget is a stateless widget that fetches and displays
+/// a list of sample dishes that can be made with chicken. It uses the `ApiService`
+/// to fetch the data and displays it in a grid format. If there is an error during
+/// the fetch operation or if the data is still loading, appropriate views are shown.
+///
+/// This widget requires an instance of `ApiService` to be passed as a required parameter.
+///
+/// Example usage:
+/// ```dart
+/// DefaultRecipesSection(apiService: myApiServiceInstance);
+/// ```
+///
+/// The widget consists of:
+/// - A title indicating the type of dishes being displayed.
+/// - A `FutureBuilder` that handles the asynchronous fetching of data.
+/// - A `LoadingView` that is displayed while the data is being fetched.
+/// - An `ErrorView` that is displayed if there is an error during the fetch operation.
+/// - A `MealGrid` that displays the fetched meals in a grid format.
+///
+/// When a meal is selected from the grid, the user is navigated to the `RecipePage`
+/// for that specific meal.
+
 import 'package:flutter/material.dart';
 import 'package:recipe_explorer/widgets/error/error_view.dart';
 import '../../../services/api_service.dart';
