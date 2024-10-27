65b61e0eb4ec600653d64fa903ab09cc1fdfa140
KatayR <rasitarslan@outlook.com>
2024-10-27 19:08:46 +0300

    Used common Error View widget instead of creating a new one

--- lib/screens/home/widgets/default_recipes.dart
+++ lib/screens/home/widgets/default_recipes.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_explorer/widgets/error/error_view.dart';
 import '../../../services/api_service.dart';
 import '../../../widgets/loading/loading_view.dart';
 import '../../../widgets/meal/meal_grid.dart';
 import '../../recipe/recipe_page.dart';
-import '../../../utils/error_handler.dart';
 
 class DefaultRecipesSection extends StatelessWidget {
   final ApiService apiService;
@@ -30,8 +30,8 @@ class DefaultRecipesSection extends StatelessWidget {
                 return const LoadingView();
               }
               if (snapshot.hasError || (snapshot.data?.error != null)) {
-                return ErrorHandler.buildErrorWidget(
-                    snapshot.data?.error ?? 'Error loading recipes');
+                return ErrorView(
+                    errString: snapshot.data?.error ?? 'Error loading recipes');
               }
               final meals = snapshot.data?.data ?? [];
               return MealGrid(
