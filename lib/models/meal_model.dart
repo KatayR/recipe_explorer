ad5d4083cddec600dd1de692a36862af543fd45b
KatayR <rasitarslan@outlook.com>
2024-10-27 20:37:37 +0300

    Added comments

--- lib/models/meal_model.dart
+++ lib/models/meal_model.dart
@@ -1,12 +1,12 @@
 class Meal {
-  final String idMeal;
-  final String strMeal;
-  final String strCategory;
-  final String strArea;
-  final String strInstructions;
-  final String strMealThumb;
-  final List<String> ingredients;
-  final List<String> measures;
+  final String idMeal; // Unique identifier for the meal
+  final String strMeal; // Name of the meal
+  final String strCategory; // Category of the meal
+  final String strArea; // Cuisine/region of origin
+  final String strInstructions; // Cooking instructions
+  final String strMealThumb; // URL for meal thumbnail image
+  final List<String> ingredients; // List of ingredients
+  final List<String> measures; // List of corresponding measurements
 
   Meal({
     required this.idMeal,
@@ -19,7 +19,8 @@ class Meal {
     required this.measures,
   });
 
-  // Factory constructor for creating a Meal object from JSON
+  /// Helper function to extract ingredients from JSON
+  /// Processes up to 20(due to hard-coded API limit) ingredients, skipping empty ones
   factory Meal.fromJson(Map<String, dynamic> json) {
     List<String> extractIngredients(Map<String, dynamic> json) {
       List<String> ingredients = [];
@@ -32,6 +33,8 @@ class Meal {
       return ingredients;
     }
 
+    /// Helper function to extract measurements from JSON
+    /// Processes up to 20(due to hard-coded API limit) measurements, skipping empty ones
     List<String> extractMeasures(Map<String, dynamic> json) {
       List<String> measures = [];
       for (int i = 1; i <= 20; i++) {
@@ -65,6 +68,7 @@ class Meal {
       'strMealThumb': strMealThumb,
     };
 
+    // Add ingredients and measurements to JSON
     for (int i = 0; i < ingredients.length; i++) {
       data['strIngredient${i + 1}'] = ingredients[i];
       data['strMeasure${i + 1}'] = measures[i];
