e10a7646ae4e65b3b7bb74e28238988e7cfbaa2b
KatayR <rasitarslan@outlook.com>
2025-06-23 23:02:18 +0300

    Convert FavoritesPage to GetView - Changed from StatefulWidget to GetView<FavoritesController> - Replaced setState() with reactive Obx() wrapper - Uses controller.favorites and controller.isLoading directly - Automatic UI updates when favorites change

--- lib/screens/favorites/favorites_page.dart
+++ lib/screens/favorites/favorites_page.dart
@@ -16,6 +16,7 @@
 /// The favorite meals are displayed using the `MealGrid` widget, which
 /// expects the meals data in a map format.
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:recipe_explorer/constants/text_constants.dart';
 import 'package:recipe_explorer/constants/ui_constants.dart';
 import 'package:recipe_explorer/widgets/loading/loading_view.dart';
@@ -26,129 +27,102 @@ import '../../widgets/meal/meal_grid.dart';
 import '../../widgets/scroll/scrollable_wrapper.dart';
 import '../recipe/recipe_page.dart';
 
-class FavoritesPage extends StatefulWidget {
+class FavoritesPage extends GetView<FavoritesController> {
   const FavoritesPage({super.key});
 
   @override
-  State<FavoritesPage> createState() => _FavoritesPageState();
-}
-
-class _FavoritesPageState extends State<FavoritesPage> {
-  final FavoritesService _favoritesService = FavoritesService();
-  final ScrollController _scrollController = ScrollController();
-  ScrollPreloader? _scrollPreloader;
-  List<Meal> _favoriteMeals = [];
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadFavorites();
-  }
-
-  @override
-  void dispose() {
-    _scrollPreloader?.dispose();
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _loadFavorites() async {
-    setState(() => _isLoading = true);
-    final meals = await _favoritesService.getFavorites();
-    setState(() {
-      _favoriteMeals = meals;
-      _isLoading = false;
-    });
-    
-    // Initialize preloading after favorites are loaded
-    if (_favoriteMeals.isNotEmpty) {
-      await _initializePreloading();
-    }
-  }
-
-  Future<void> _initializePreloading() async {
-    if (_favoriteMeals.isEmpty) return;
-
-    // Extract image URLs from favorite meals
-    final imageUrls = _favoriteMeals
-        .map((meal) => meal.strMealThumb)
-        .where((url) => url.isNotEmpty)
-        .toList();
-
-    // Initialize scroll preloader service
-    _scrollPreloader = ScrollPreloader(imageUrls: imageUrls);
-    await _scrollPreloader!.initialize(context);
-
-    // Set up scroll listener to delegate to the service
-    _scrollController.addListener(() {
-      _scrollPreloader!.onScroll(_scrollController);
-    });
-  }
-
-  void _onMealSelected(Meal meal) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => RecipePage(
-          mealId: meal.idMeal,
-          mealName: meal.strMeal,
-        ),
-      ),
-    ).then((_) => _loadFavorites()); // Refreshing list when returning
-  }
-
-  Widget _buildContent() {
-    if (_isLoading) {
-      return const LoadingView();
-    }
-
-    if (_favoriteMeals.isEmpty) {
-      return const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.favorite_border,
-              size: 64,
-              color: Colors.grey,
-            ),
-            SizedBox(height: UIConstants.defaultSpacing),
-            Text(
-              TextConstants.noFavoritesMessage,
-              style: TextStyle(
-                fontSize: UIConstants.bodyFontSize,
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: UIConstants.defaultPadding),
-            Text(
-              TextConstants.addFavoritesMessage,
-              style: TextStyle(
-                color: Colors.grey,
-              ),
-            ),
-          ],
-        ),
-      );
-    }
-
-    final mealsData = _favoriteMeals.map((meal) => meal.toJson()).toList();
-
-    return MealGrid(
-      meals: mealsData,
-      onMealSelected: _onMealSelected,
-      scrollController: _scrollController,
-      useCachedImages: true,
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final ScrollController scrollController = ScrollController();
+    ScrollPreloader? scrollPreloader;
+
+    // Initialize preloading when favorites are loaded
+    void initializePreloading() async {
+      if (controller.favorites.isEmpty) return;
+
+      // Extract image URLs from favorite meals
+      final imageUrls = controller.favorites
+          .map((meal) => meal.strMealThumb)
+          .where((url) => url.isNotEmpty)
+          .toList();
+
+      // Initialize scroll preloader service
+      scrollPreloader = ScrollPreloader(imageUrls: imageUrls);
+      await scrollPreloader!.initialize(context);
+
+      // Set up scroll listener to delegate to the service
+      scrollController.addListener(() {
+        scrollPreloader!.onScroll(scrollController);
+      });
+    }
+
+    void onMealSelected(Meal meal) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => RecipePage(
+            mealId: meal.idMeal,
+            mealName: meal.strMeal,
+          ),
+        ),
+      ).then((_) => controller.loadFavorites()); // Refreshing list when returning
+    }
+
+    Widget buildContent() {
+      return Obx(() {
+        if (controller.isLoading.value) {
+          return const LoadingView();
+        }
+
+        if (controller.favorites.isEmpty) {
+          return const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.favorite_border,
+                  size: 64,
+                  color: Colors.grey,
+                ),
+                SizedBox(height: UIConstants.defaultSpacing),
+                Text(
+                  TextConstants.noFavoritesMessage,
+                  style: TextStyle(
+                    fontSize: UIConstants.bodyFontSize,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: UIConstants.defaultPadding),
+                Text(
+                  TextConstants.addFavoritesMessage,
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+
+        final mealsData = controller.favorites.map((meal) => meal.toJson()).toList();
+
+        // Initialize preloading when favorites are available
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          initializePreloading();
+        });
+
+        return MealGrid(
+          meals: mealsData,
+          onMealSelected: onMealSelected,
+          scrollController: scrollController,
+          useCachedImages: true,
+        );
+      });
+    }
+
     return ScrollableWrapper(
-      controller: _scrollController,
+      controller: scrollController,
       title: TextConstants.favoritesTitle,
-      child: _buildContent(),
+      child: buildContent(),
     );
   }
 }
